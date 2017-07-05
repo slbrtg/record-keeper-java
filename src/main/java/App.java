fb560944200d605f5e797524a397842ff5678328
Saul Artiga <slbrtg@gmail.com>
2017-07-05 14:43:07 -0700

    create template for collection page, add html and css to page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,12 +9,17 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-    get("/", (request, response) ->{
+    get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/collection.vtl", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/collection.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
 }
