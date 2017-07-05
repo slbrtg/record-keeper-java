898fa7bbd1963b80e8ac145710a23ee3ade2fdaa
Saul Artiga <slbrtg@gmail.com>
2017-07-05 15:46:51 -0700

    add functioning route for collection.vtl as well as post method for writing albums titles to the page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,8 +15,20 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/collection.vtl", (request, response) -> {
+    get("/collection", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/collection.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/collection", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String artist = request.queryParams("artist");
+      String album = request.queryParams("album");
+      Record newRecord = new Record(artist, album);
+      model.put("allRecords", Record.all());
+      model.put("artist", newRecord.getArtist());
+      model.put("album", newRecord.getAlbum());
       model.put("template", "templates/collection.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
